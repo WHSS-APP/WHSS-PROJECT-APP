4bfe405a10e0711e8c8f95d616e56c7d534037bf
naemsupawatt <naemsupawat@gmail.com>
2023-09-30 19:27:41 +0700

    [UPDATE] Create List View

--- lib/screens/equipment_inspection_record.dart
+++ lib/screens/equipment_inspection_record.dart
@@ -32,200 +32,195 @@ class _EquipmentInspectionRecordState extends State<EquipmentInspectionRecord> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Column(children: [
-          TextField(
-            decoration: InputDecoration(
-              hintText: '',
-              border: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.grey),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              // helperText: 'Keep it short, this is just a demo.',
-              labelText: 'ค้นหาหมายเลขภาพ, ตำแหน่ง...',
-              prefixIcon: const Icon(
-                Icons.search,
-                color: Colors.grey,
-              ),
-            ),
-          ),
-          Expanded(
-              child: ListView(
-            scrollDirection: Axis.vertical,
+        child: SingleChildScrollView(
+          child: Column(
             children: [
-              InkWell(
-                child: SingleChildScrollView(
-                  child: Center(
-                      child: Column(
-                    children: [
-                      SizedBox(
-                        height: 10,
+              SizedBox(height: 10),
+              Container(
+                decoration: BoxDecoration(
+                  color: Color(0xFFffffff),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 15.0, // soften the shadow
+                      spreadRadius: 5.0, //extend the shadow
+                      offset: Offset(
+                        5.0, // Move to right 5  horizontally
+                        5.0, // Move to bottom 5 Vertically
                       ),
-                      Card(
-                        child: Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: ListTile(
-                            leading: ConstrainedBox(
-                                constraints: BoxConstraints(
-                                  minWidth: 44,
-                                  minHeight: 44,
-                                  maxWidth: 64,
-                                  maxHeight: 44,
-                                ),
-                                child: Image.network(
-                                  'https://picsum.photos/250?image=9',
-                                )),
-                            title: Column(children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                  color: Color.fromRGBO(21, 29, 40, 1),
-                                  borderRadius: BorderRadius.circular(
-                                      10.0), // เพิ่มค่า border radius ตรงนี้
-                                ),
-                                child: Padding(
-                                  padding:
-                                      const EdgeInsets.fromLTRB(25, 8, 25, 8),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text(
-                                        "ภาพที่",
-                                        style: TextStyle(
-                                          fontFamily: 'Prompt',
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 16,
-                                          color: Color.fromRGBO(255, 153, 0, 1),
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        width: 80,
-                                      ),
-                                      Text(
-                                        "K20161027091905",
-                                        style: TextStyle(
-                                          fontFamily: 'Prompt',
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 16,
-                                          color: Color.fromARGB(
-                                              255, 255, 255, 255),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Container(
-                                decoration: BoxDecoration(
-                                  color: Color.fromRGBO(21, 29, 40, 1),
-                                  borderRadius: BorderRadius.circular(
-                                      10.0), // เพิ่มค่า border radius ตรงนี้
-                                ),
-                                child: Padding(
-                                  padding:
-                                      const EdgeInsets.fromLTRB(25, 8, 25, 8),
-                                  child: Row(
-                                    children: [
-                                      Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            "ตำแหน่ง",
-                                            style: TextStyle(
-                                                fontFamily: 'Prompt',
-                                                fontWeight: FontWeight.bold,
-                                                fontSize: 16,
-                                                color: Color.fromRGBO(
-                                                    255, 255, 255, 1)),
-                                          ),
-                                          Text(
-                                            "ชั้นที่",
-                                            style: TextStyle(
-                                                fontFamily: 'Prompt',
-                                                fontWeight: FontWeight.bold,
-                                                fontSize: 16,
-                                                color: Color.fromRGBO(
-                                                    255, 255, 255, 1)),
-                                          ),
-                                          Text(
-                                            "ความเสียหาย",
-                                            style: TextStyle(
-                                                fontFamily: 'Prompt',
-                                                fontWeight: FontWeight.bold,
-                                                fontSize: 16,
-                                                color: Color.fromRGBO(
-                                                    255, 255, 255, 1)),
-                                          ),
-                                          Text(
-                                            "การแก้ไข",
-                                            style: TextStyle(
-                                                fontFamily: 'Prompt',
-                                                fontWeight: FontWeight.bold,
-                                                fontSize: 16,
-                                                color: Color.fromRGBO(
-                                                    255, 255, 255, 1)),
-                                          )
-                                        ],
-                                      ),
-                                      SizedBox(
-                                        width: 30,
-                                      ),
-                                      Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            "ตำแหน่ง",
-                                            style: TextStyle(
-                                                fontFamily: 'Prompt',
-                                                fontSize: 16,
-                                                color: Color.fromRGBO(
-                                                    255, 255, 255, 1)),
-                                          ),
-                                          Text(
-                                            "ชั้นที่",
-                                            style: TextStyle(
-                                                fontFamily: 'Prompt',
-                                                fontSize: 16,
-                                                color: Color.fromRGBO(
-                                                    255, 255, 255, 1)),
-                                          ),
-                                          Text(
-                                            "ความเสียหาย",
-                                            style: TextStyle(
-                                                fontFamily: 'Prompt',
-                                                fontSize: 16,
-                                                color: Color.fromRGBO(
-                                                    255, 255, 255, 1)),
-                                          ),
-                                          Text(
-                                            "การแก้ไข",
-                                            style: TextStyle(
-                                                fontFamily: 'Prompt',
-                                                fontSize: 16,
-                                                color: Color.fromRGBO(
-                                                    255, 255, 255, 1)),
-                                          )
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ]),
-                          ),
-                        ),
-                      )
-                    ],
-                  )),
+                    )
+                  ],
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
-              )
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: '',
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    // helperText: 'Keep it short, this is just a demo.',
+                    labelText: 'ค้นหาหมายเลขภาพ, ตำแหน่ง...',
+                    prefixIcon: const Icon(
+                      Icons.search,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFffffff),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 15.0, // soften the shadow
+                          spreadRadius: 5.0, //extend the shadow
+                          offset: Offset(
+                            5.0, // Move to right 5  horizontally
+                            5.0, // Move to bottom 5 Vertically
+                          ),
+                        )
+                      ],
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(children: [
+                        Column(
+                          children: [
+                            Image.network(
+                              'https://picsum.photos/250?image=9',
+                              width: 130,
+                              height: 130,
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 290,
+                              color: Color.fromRGBO(21, 29, 40, 1),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "ภาพที่",
+                                      style: TextStyle(
+                                          color: Color.fromRGBO(255, 153, 0, 1),
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      "K20161027091905",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 5),
+                            Container(
+                              width: 290,
+                              color: Color.fromRGBO(21, 29, 40, 1),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          "ตำแหน่ง",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Text(
+                                          "H13",
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          "ชั้นที่",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Text(
+                                          "N",
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          "ความเสียหาย",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Text(
+                                          "Base Plate เป็นสนิม",
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          "การแก้ไข",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Text(
+                                          "ซ่อม",
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                      ]),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
             ],
-          ))
-        ]),
+          ),
+        ),
       ),
     );
   }
